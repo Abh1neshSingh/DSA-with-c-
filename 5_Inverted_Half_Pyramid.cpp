e761b8d351193d77efb7f72486da1404b5fc01e1
Abhinesh Singh <152651258+Abh1neshSingh@users.noreply.github.com>
2024-02-12 23:40:35 +0530

    Add files via upload

---
+++ 5_Inverted_Half_Pyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number for print Inverted Half Pyrimd " << endl;
+    cin >> n;
+
+    for (int row = 0; row < n; row = row + 1)
+    {
+        for (int col = 0; col < n - row; col = col + 1)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
